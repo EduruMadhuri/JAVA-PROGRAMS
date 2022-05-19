2322cd08ad6d392fba3b4a6ae639792c5667b1b4
EduruMadhuri <104362718+EduruMadhuri@users.noreply.github.com>
2022-05-19 23:23:36 +0530

    Add files via upload

---
+++ Continue.java
@@ -0,0 +1,17 @@
+class Continue
+  {
+    public static void main(String args[])
+    {
+      int i;
+      for(i=0;i<=10;i++)
+        {
+          if(i==6||i==7)
+          {
+          continue;
+          }
+          System.out.print(i);
+          }
+      }
+  }
+          
+    
